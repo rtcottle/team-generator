7c0c56a3195f92462ae0d3665aab904f3c59ed6f
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-11-17 18:29:21 -0700

    added comments and HTML

--- README.md
+++ README.md
@@ -33,8 +33,9 @@ Link to video of this program working: https://drive.google.com/file/d/1CFXFXSCE
 
 ## Credits
 
-<!-- - README formatting: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide/
-- Inquirer: https://www.npmjs.com/package/inquirer -->
+- README formatting: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide/
+- Inquirer: https://www.npmjs.com/package/inquirer
+- Bulma: https://bulma.io/
 
 ## License
 
---
+++ index.js
@@ -0,0 +1,22 @@
+//TODO: import employees
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Employee = require("./lib/employee");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//TODO: generate HTML file
+const htmlFile = `
+
+`;
+
+//TODO: generate CSS file
+//TODO: clicking on HTML link for email opens email in new window with to field as the email address
+//TODO: clicking on github link opens in new tab
+//TODO: start application, asked to put in Manager info
+
+//TODO: this needs to be series of inquirer prompts.
+//TODO: Option to add intern or engineer
+//TODO: also a series of inquirer prompts depending on what type of employee they put in
+//TODO: html generates at the end with team roster
